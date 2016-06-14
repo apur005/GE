4304cf040f33917e43552e0486145dab22eb9acf
apur005 <apur005@gmail.com>
2016-06-14 14:50:38 +0530

    Update mongo.rb

--- GE/recipes/mongo.rb
+++ GE/recipes/mongo.rb
@@ -9,9 +9,40 @@
 #Installing updates
 
 execute 'update' do
-  command 'apt-get install update'
+  command 'apt-get update'
 end
 
 execute '"Installing MongoDB"' do
 command 'apt-get install -y mongodb-org=3.0.9 mongodb-org-server=3.0.9 mongodb-org-shell=3.0.9 mongodb-org-mongos=3.0.9 mongodb-org-tools=3.0.9'
+end
+
+service "mongod" do
+  action :stop
+end
+
+service "mongod" do
+  action :start
+end
+
+bash 'Setting up MongoDB' do
+code <<-EOH
+cd $HOME/golden-eye/deploy/data/db/data
+mongoimport --db clt --collection locations --file clt_locations.json --drop --stopOnError -v --username admin --password M0ngo@1501007 --authenticationDatabase admin
+mongoimport --db tg --collection locations --file tg_locations.json --drop --stopOnError -v --username admin --password M0ngo@1501007 --authenticationDatabase admin
+mongoimport --db ibibo --collection locations --file ibibo_locations.json --drop --stopOnError -v --username admin --password M0ngo@1501007 --authenticationDatabase admin
+mongoimport --db booking --collection locations --file booking_locations.json --drop --stopOnError -v --username admin --password M0ngo@1501007 --authenticationDatabase admin
+mongoimport --db ex --collection locations --file ex_locations.json --drop --stopOnError -v --username admin --password M0ngo@1501007 --authenticationDatabase admin
+mongoimport --db common --collection unified_hotels --file common_unified_hotels.json --drop --stopOnError -v --username admin --password M0ngo@1501007 --authenticationDatabase admin
+mongoimport --db common --collection users --file common_users.json --drop --stopOnError -v --username admin --password M0ngo@1501007 --authenticationDatabase admin
+mongoimport --db common --collection currencies --file common_currencies.json --drop --stopOnError --username admin --password M0ngo@1501007 --authenticationDatabase admin
+EOH
+end
+bash 'Setting up MongoDB' do
+code <<-EOH
+cd $HOME/golden-eye/deploy/data/db/data
+cd $HOME/golden-eye/deploy/data/db/users
+mongo admin admin.js
+mongo admin reader.js
+mongo common common.js
+EOH
 end
